814008dd12686d83bce2ea6091845ac144ff008a
John Kang <pollyfactor@hotmail.com>
2020-07-13 13:04:16 -0400

    fixed charname

--- client/src/components/Shared/Card/Card.jsx
+++ client/src/components/Shared/Card/Card.jsx
@@ -13,9 +13,9 @@ const Card = (props) => {
         {/* <h5 className="card-title">{props.title}</h5> */}
         {/* <p className="card-text">{props.desc}</p> */}
         {/* <Link to={props.url} className={location.pathname === props.url ? "nav-link active" : "nav-link"}> */}
-        <Link to={props.url} charName={props.charName}>
-        <button className="btn btn-primary" onClick={props.onClick} charName={props.charName}>
-          {props.charName}
+        <Link to={props.url} charname={props.charname}>
+        <button className="btn btn-primary" onClick={props.onClick} charname={props.charname}>
+          {props.charname}
         </button>
         </Link>
       </div>
