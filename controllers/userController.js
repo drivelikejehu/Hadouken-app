6462704229fcb4d5369c8242eed703af464e376a
Ricardo Russ <ricardoruss@live.com>
2020-07-21 16:16:25 -0400

    signin sends data

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,16 +8,12 @@ router.post("/", (req, res) => {
   const email = req.body.email.trim();
   const password = req.body.password.trim();
   const username = req.body.username.trim();
-  const birthday = req.body.birthday.trim();
-  const picURl = req.body.picURl.trim();
   db.User.create({
     firstName,
     lastName,
     email,
     password,
     username,
-    picURl,
-    birthday,
   })
     .then(() => {
       res.json({
@@ -47,9 +43,9 @@ router.get("/username/:username", (req, res) => {
 
 router.put(":/id", (req, res) => {
   const updateUser = {
-    username: req.body.username.trim(),
-    birthday: req.body.birthday.trim(),
-    picURl: req.body.picURl.trim(),
+    username: req.body.username,
+    birthday: req.body.birthday,
+    picURl: req.body.picURl,
   };
   db.User.update(updateUser, {
     where: {
